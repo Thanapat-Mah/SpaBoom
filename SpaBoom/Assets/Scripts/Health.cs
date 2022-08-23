b9a214d2d7d9a296a230bb3db79b65fd6f8ba491
Thanapat-Mah <thanapat.mah@mail.kmutt.ac.th>
2022-08-23 16:56:07 +0700

    Initiate player's health system and UI

---
+++ SpaBoom/Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Image[] hearts;
+    public int remainingHealth = 3;
+    
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < remainingHealth)
+            {
+                hearts[i].color = Color.red;
+            } else {
+                hearts[i].color = Color.white;
+            }
+        }
+    }
+}
