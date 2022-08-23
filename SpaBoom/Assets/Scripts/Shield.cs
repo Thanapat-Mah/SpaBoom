2e8e6984bfd264787db4bf894412d39ad0c5c4c8
Thanapat-Mah <thanapat.mah@mail.kmutt.ac.th>
2022-08-23 15:59:49 +0700

    Create rotatable shield

---
+++ SpaBoom/Assets/Scripts/Shield.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{ 
+    void Start()
+    {
+    }
+ 
+    void Update()
+    {
+        // calculate angle to rotate
+        Vector3 orbVector = Camera.main.WorldToScreenPoint(transform.position);
+        orbVector = Input.mousePosition - orbVector;
+        float angle = Mathf.Atan2(orbVector.y, orbVector.x) * Mathf.Rad2Deg;
+ 
+        // rotate the object according to calculated angle
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
