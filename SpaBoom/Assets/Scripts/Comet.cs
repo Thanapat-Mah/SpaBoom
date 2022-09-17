23b115372c69644e26f3aba7e8fd3f40afffd648
PNWhy5 <n.pojnarin5@gmail.com>
2022-09-17 19:22:56 +0700

    Hotfix cometStar

--- SpaBoom/Assets/Scripts/Comet.cs
+++ SpaBoom/Assets/Scripts/Comet.cs
@@ -32,6 +32,9 @@ public class Comet : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("PlayerBullet"))
         {
+            if(this.gameObject.CompareTag("CometStar")){
+                ScoreManager.Instance.AddScore(100);
+            }
             onDestroyed.Invoke();   // Invoke a delegate or a function subscript to this event.
             Destroy(gameObject);
         }
